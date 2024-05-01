55e7b06e0b63b658317b5ae9b407673c84faa229
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-05-01 13:13:51 +0530

    Added different Sections

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,21 @@
-
 import React, { useContext, useEffect, useState } from "react";
 import ProductCards from "./ProductCards";
 import ProductContext from "../Context/Product";
 import Sections from "./Sections";
 
 function Home() {
-  const [sectionData,setData] = useState([
-    {sec : `Today's`, title : "Flash Sales"},
-    {sec : `Categories`, title : "Browse By Category"},
-    {sec : `This Month`, title : "Best Selling Products"},
-    {sec : `Our Products`, title : "Explore Our Products"},
-  ])
-  const {productsData,setProductData} = useContext(ProductContext);
+  const [sectionData, setData] = useState([
+    { sec: `Today's`, title: "Flash Sales" },
+    { sec: `Men's`, title: "Men's Clothings" },
+    { sec: `Women's`, title: "Women's Clothings" },
+    { sec: `This Month`, title: "Best Selling Products" },
+    { sec: `Our Products`, title: "Explore Our Products" },
+  ]);
+  const { productsData, setProductData } = useContext(ProductContext);
 
-  const topRatted = productsData.filter((elem)=> elem.rating.rate > 4);
+  const topRatted = productsData.filter((elem) => elem.rating.rate > 4);
+  const mens = productsData.filter((elem) => elem.category == `men's clothing`);
+  const womens = productsData.filter((elem) => elem.category == `women's clothing`);
 
   return (
     <div className="w-full h-fit p-4 relative ">
@@ -22,28 +24,48 @@ function Home() {
         src="./public/hero.avif"
         alt=""
       />
-      <section className="todays w-full mb-12">
+      <section className="todays w-full mb-12 flex flex-col  gap-4">
+        {sectionData.map((elem) => {
+          if (elem.sec === `Today's`) return <Sections data={elem} />;
+        })}
 
-     {sectionData.map((elem)=>{
-       if(elem.sec === `Today's`) return <Sections data={elem}/>
-      })}
-
-
-      <div className=" flex items-center px-8 overflow-x-auto m-4 gap-5">
-      {topRatted.map((elem,index)=><ProductCards key={elem.id} value={elem}/>)}
-      </div>
-  
+        <div className=" flex items-center px-8 overflow-x-auto m-4 gap-6">
+          {topRatted.map((elem, index) => (
+            <ProductCards key={elem.id} value={elem} />
+          ))}
+        </div>
+        <button className="bg-[#db4444] px-4 py-2 mx-auto text-white font-bold rounded-sm w-fit">
+          View All Products
+        </button>
       </section>
 
-      <section>
-
-      {sectionData.map((elem)=>{
-        if(elem.sec === `Categories`) return <Sections data={elem}/>
-      })}
+      <section className="category w-full mb-12 flex flex-col  gap-4">
+        {sectionData.map((elem) => {
+          if (elem.sec === `Men's`) return <Sections data={elem} />;
+        })}
+        <div className=" flex items-center px-8 overflow-x-auto m-4 gap-6">
+          {mens.map((elem, index) => (
+            <ProductCards key={elem.id} value={elem} />
+          ))}
+        </div>
+        <button className="bg-[#db4444] px-4 py-2 mx-auto text-white font-bold rounded-sm w-fit">
+          View All Products
+        </button>
       </section>
 
-
-      
+      <section className="category w-full mb-12 flex flex-col  gap-4">
+        {sectionData.map((elem) => {
+          if (elem.sec === `Women's`) return <Sections data={elem} />;
+        })}
+        <div className=" flex items-center px-8 overflow-x-auto m-4 gap-6">
+          {womens.map((elem, index) => (
+            <ProductCards key={elem.id} value={elem} />
+          ))}
+        </div>
+        <button className="bg-[#db4444] px-4 py-2 mx-auto text-white font-bold rounded-sm w-fit">
+          View All Products
+        </button>
+      </section>
     </div>
   );
 }
--- src/Components/Sections.jsx
+++ src/Components/Sections.jsx
@@ -4,8 +4,8 @@ function Sections({data}) {
   return (
     <div className='w-full'> 
         <div className='items-center flex gap-3'>
-            <div className='w-5 h-12 bg-[#db4444] rounded-md'></div>
-            <h6 className='text-[#db4444] text-xl font-bold'>{data.sec}</h6>
+            <div className='w-5 h-14 bg-[#db4444] rounded-md'></div>
+            <h6 className='text-[#db4444] text-2xl font-bold'>{data.sec}</h6>
         </div>
         <h1 className='text-4xl font-semibold mt-4'>{data.title}</h1>
 
